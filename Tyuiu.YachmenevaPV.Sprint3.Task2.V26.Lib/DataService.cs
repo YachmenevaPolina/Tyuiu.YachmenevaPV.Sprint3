6b320379e76e4903b4e8ef6ff6678a53d9cd3306
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-10-30 19:11:20 +0500

    Task2

---
+++ Tyuiu.YachmenevaPV.Sprint3.Task2.V26.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.YachmenevaPV.Sprint3.Task2.V26.Lib
+{
+    public class DataService : ISprint3Task2V26
+    {
+        public double GetMultiplySeries(double value, int startValue, int stopValue)
+        {
+            double p = 1;
+            int i = startValue;
+            do
+            {
+                p *= (Math.Pow(value, 3) * i + 2);
+                i++;
+            }
+            while (i <= stopValue);
+            return Math.Round(p, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint3.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.YachmenevaPV.Sprint3.Task2.V26.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint3.Task2.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidMultiplySeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 17;
+
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 411589.537;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
