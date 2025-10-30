30059a6865e853dad8c1634b2a8ecb698485cc27
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-10-30 17:46:54 +0500

    Task1

--- Tyuiu.YachmenevaPV.Sprint3.Task1.V4.Lib/DataService.cs
+++ Tyuiu.YachmenevaPV.Sprint3.Task1.V4.Lib/DataService.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.YachmenevaPV.Sprint3.Task1.V4.Lib
             int k = 1;
             while (k <= 10)
             {
-                sum = sum + Math.Pow((1 / k), 2);
+                sum = sum + Math.Pow((1.0 / k), 2);
                 k++;
             }
             return Math.Round(sum, 3);
--- Tyuiu.YachmenevaPV.Sprint3.Task1.V4.Test/DataServiceTest.cs
+++ Tyuiu.YachmenevaPV.Sprint3.Task1.V4.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.YachmenevaPV.Sprint3.Task1.V4.Test
             int startValue = 1;
             int stopValue = 10;
             double res = ds.GetSumSeries(startValue, stopValue);
-            double wait = 1;
+            double wait = 1.55;
             Assert.AreEqual(wait, res);
         }
     }
